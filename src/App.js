31feff064b7953a017219557ba742d3b9105690b
yesolll <9595kys@gmail.com>
2021-04-27 11:32:52 +0900

    상위 state와 하위 props를 이용한 코드 개선

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ import Subject from './components/Subject';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: 'WEB', sub: 'World Wide Web!' },
+      contents: [
+        { id: 1, title: 'HTML', desc: 'HTML is For ' },
+        { id: 2, title: 'CSS', desc: 'CSS is for design' },
+        { id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive' },
+      ],
+    };
+  }
   render() {
     return (
       <div className='App'>
-        <Subject title='WEB' sub='world wide web!'></Subject>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
         <Subject title='React' sub='For UI'></Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC>
         <Content title='HTML' dsc='HTML is HyperText Markup Language.'></Content>
       </div>
     );
