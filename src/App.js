6e486b682e249c0c28513d8c7a60649061ef7894
yesolll <9595kys@gmail.com>
2021-04-27 14:35:37 +0900

    evnet 설치 (WEB을 누르면 mode가 변경되게어 출력내용이 바뀌게)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode: 'null',
+      mode: 'read',
       subject: { title: 'WEB', sub: 'World Wide Web!' },
       welcome: { title: 'Welcome', desc: 'Hello, React!' },
       contents: [
@@ -28,9 +28,29 @@ class App extends Component {
       _title = this.state.contents[0].title;
       _desc = this.state.contents[0].desc;
     }
+    console.log('render', this);
     return (
       <div className='App'>
-        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        {/* <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject> */}
+        <header>
+          <h1>
+            <a
+              href='/'
+              onClick={function (e) {
+                console.log(e);
+                e.preventDefault();
+                // this.state.mode = 'welcome'; : this에 아무 값도 없어 state를 읽을 수 없음 -> bind(this)를 함수 끝난 직후에 추가해주기 -> 그럼 this가 본 컴포넌트가 됨! + 문법에도 맞지 않음
+                this.setState({
+                  // 요렇게 직접 변경 말고 함수 형태로 변경해야 함
+                  mode: 'welcome',
+                });
+              }.bind(this)}
+            >
+              {this.state.subject.title}
+            </a>
+          </h1>
+          {this.state.subject.sub}
+        </header>
         <TOC data={this.state.contents}></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
